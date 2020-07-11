58cd7cd4c1baccc2ee9161230801432432dd0657
Waweru-Steve <wawerusteve142@yahoo.com>
2020-07-11 12:36:11 +0300

    Added Authentication controllers/routes

---
+++ app/Notifications/VerifyEmail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Notifications;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Auth\Notifications\VerifyEmail as Notification;
+
+class VerifyEmail extends Notification
+{
+ protected function  verificationUrl($notifiable) {
+     $appUrl = config('app.client_url', config('app_url'));
+
+     $url = URL::temporarySignedRoute('verification.verify', Carbon::now()->addMinutes(60),
+     ['user' => $notifiable->id]);
+
+     // http://designhouse.test/api/ddjjd
+     return str_replace(url('/api'), $appUrl, $url);
+ }
+}
